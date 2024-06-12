5b60b39664e2057d273e3c3c4929783cf71fc37e
melch <clement.passet@gmail.com>
2024-06-12 09:04:21 +0200

    server side : updating Topic model

--- back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
@@ -1,38 +1,53 @@
 package com.openclassrooms.mddapi.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "topics")
+@Data
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor
 public class Topic {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "topic_id")
 	private Long id;
-	
-	@Column(nullable = false)
+
+	@NonNull
 	private String name;
 
-	public Long getId() {
-		return id;
-	}
+	@Column(name = "created_at")
+	@CreatedDate
+	private LocalDateTime createdAt;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+	@ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+	@JoinTable(
+		name="topics_posts",
+		joinColumns = @JoinColumn(name = "topic_id"),
+		inverseJoinColumns = @JoinColumn(name = "post_id")
+	)
+	private List<Post> posts;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 }
