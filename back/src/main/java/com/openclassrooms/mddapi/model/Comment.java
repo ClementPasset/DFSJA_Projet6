b7628ace40077e0b66ac1afda3e97c0e6cb5891c
melch <clement.passet@gmail.com>
2024-06-12 09:02:48 +0200

    server side : finishing Comment model

--- back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
@@ -1,29 +1,50 @@
 package com.openclassrooms.mddapi.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "comments")
+@Data
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "comment_id")
 	private Long id;
-	
-	// TODO : to finish...
 
-	public Long getId() {
-		return id;
-	}
+	@NonNull
+	private String content;
 
-	public void setId(Long id) {
-		this.id = id;
-	}	
-	
+	@CreatedDate
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.PERSIST }, fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
+
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST }, fetch = FetchType.LAZY)
+	@JoinColumn(name = "author_id")
+	private User author;
 }
