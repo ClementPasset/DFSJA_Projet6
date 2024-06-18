07541a543e83b948d46ccd30d71c83e925f8c537
melch <clement.passet@gmail.com>
2024-06-18 13:56:55 +0200

    feat(front): adding HeaderComponent

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+
+  public displayHeader: boolean = false;
+
+  public constructor(private router: Router) { }
+
+  public ngOnInit(): void {
+    this.router.events.forEach(event => {
+      if (event instanceof NavigationStart) {
+        this.displayHeader = event.url !== "/";
+      }
+    });
+  }
 }
--- front/src/app/core/core.module.ts
+++ front/src/app/core/core.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { LandingComponent } from './components/landing/landing.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     CoreRoutingModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
