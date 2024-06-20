fa094a45e267f6c11d8f4f753e3039d277873b81
melch <clement.passet@gmail.com>
2024-06-20 21:44:12 +0200

    feat(front): updating AuthModule and AuthRoutingModule

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    AuthFormComponent,
+    LoginComponent
   ],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    CoreModule
+  ],
+  providers: [
+    AuthService
   ]
 })
 export class AuthModule { }
