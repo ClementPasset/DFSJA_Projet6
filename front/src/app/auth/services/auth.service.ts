c2dafb39bc741908a637e9e6c9f873aabe1fd5da
melch <clement.passet@gmail.com>
2024-06-20 21:39:16 +0200

    feat(front): add AuthService with registerUser method

---
+++ front/src/app/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RegisterRequest } from "../models/RegisterRequest.model";
+import { Observable, catchError, map, of } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(formValue: RegisterRequest): Observable<boolean> {
+    return this.http.post(`${environment.baseUrl}/auth/register`, formValue).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+
+}
