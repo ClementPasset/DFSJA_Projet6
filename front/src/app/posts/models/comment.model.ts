cc9ad11ff18ca87ca1b0c2bff72d1e2cb9648932
melch <clement.passet@gmail.com>
2024-06-22 14:18:37 +0200

    feat(front): adding models for posts

---
+++ front/src/app/posts/models/comment.model.ts
@@ -0,0 +1,6 @@
+export class Comment {
+    public id!: Number;
+    public content!: string;
+    public authorId!: Number;
+    public createdAt!: string;
+}
---
+++ front/src/app/posts/models/post.model.ts
@@ -0,0 +1,12 @@
+import { Comment } from "./comment.model";
+import { Topic } from "./topic.model";
+
+export class Post {
+    public id!: Number;
+    public author!: Number;
+    public title!: string;
+    public content!: string;
+    public createdAt!: string;
+    public comments?: Comment[];
+    public topics?: Topic[];
+}
