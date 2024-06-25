61bdf8b12340c835909404f00c8062cb7d6d4b09
melch <clement.passet@gmail.com>
2024-06-25 11:13:53 +0200

    feat(front): adding createComment method for PostsService

--- front/src/app/posts/services/posts.service.ts
+++ front/src/app/posts/services/posts.service.ts
@@ -1,19 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from '../models/post.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Comment } from '../models/comment.model';
 
 @Injectable()
 export class PostsService {
 
   public constructor(private http: HttpClient) { }
 
+  private _comments$ = new BehaviorSubject<Comment[]>([]);
+  get comment$(): Observable<Comment[]> {
+    return this._comments$.asObservable();
+  }
+
   public getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(`${environment.baseUrl}/post`);
   }
 
   public getPost(id: Number): Observable<Post> {
-    return this.http.get<Post>(`${environment.baseUrl}/post/${id}`);
+    return this.http.get<Post>(`${environment.baseUrl}/post/${id}`).pipe(
+      tap(post => {
+        if (post.comments) {
+          this._comments$.next(post.comments);
+        }
+      })
+    );
+  }
+
+  public createComment(content: string, postId: Number): void {
+    this.http.post<Comment>(`${environment.baseUrl}/comment`, { content, postId }).pipe(
+      tap(comment => {
+        this._comments$.next([...this._comments$.getValue(), comment]);
+      })
+    ).subscribe();
   }
 }
